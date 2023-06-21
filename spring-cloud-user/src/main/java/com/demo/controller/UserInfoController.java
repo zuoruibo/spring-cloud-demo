52c4926db666a9ba8c459ad107f055b2c832bbbe
zuoruibo <zuoruibo@qq.com>
2023-06-21 14:35:33 +0800

    init

---
+++ spring-cloud-user/src/main/java/com/demo/controller/UserInfoController.java
@@ -0,0 +1,9 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userInfo")
+public class UserInfoController {
+}
